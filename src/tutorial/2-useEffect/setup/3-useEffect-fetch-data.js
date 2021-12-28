5cd8337e6701cf2ba29f54e311118368db17c47e
Bernard <bernardy_ho@hotmail.com>
2021-12-28 21:23:08 +0700

    learning multiple returns

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -3,21 +3,25 @@ import React, { useState, useEffect } from "react";
 const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
+  // what this useState does is, set users and setUsers as an empty array
   const [users, setUsers] = useState([]);
+  // console.log("users outside getUsers", users);
   const getUsers = async () => {
     // fetching the api url
     const response = await fetch(url);
     // change to json and put it in users variable
     const users = await response.json();
 
-    // if we are re-triggering rerender
+    // what is the useState doing? it preserves the value and triggers re-render
     setUsers(users);
     // console.log(users);
   };
+
+  // this useEffect runs every render
   useEffect(() => {
     getUsers();
 
-    // make sure that we add the dependency array []
+    // make sure that we add the dependency array [] so that we dont get an infinite loop, this dependency array means only run it once
   }, []);
   return (
     <>
--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,53 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [users, setUsers] = useState("default user");
+
+  const getUsers = async () => {
+    const response = await fetch(url);
+
+    if (response.status >= 200 && response.status < 299) {
+      const users = await response.json();
+      const { login } = users;
+      setUsers(login);
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);
+      setIsError(true);
+      throw new Error(response.statusText);
+    }
+    // users here is an object, thats why we can do const {login} = users;
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <h1>Error</h1>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div>
+        <h1>{users}</h1>
+      </div>
+    </>
+  );
 };
 
 export default MultipleReturns;
